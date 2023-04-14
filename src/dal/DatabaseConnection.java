7520b6ca34a078ae9afc1d235a6d8c86f22073fe
Nuuurf <57993604+Nuuurf@users.noreply.github.com>
2023-04-14 09:45:08 +0200

    added login configuration

--- src/dal/DatabaseConnection.java
+++ src/dal/DatabaseConnection.java
@@ -14,11 +14,11 @@ public class DatabaseConnection {
 
     // Connection string for connecting to SQL Server
     private static final String CONNECTION_STRING = String.format("jdbc:sqlserver://localhost:1433;encrypt=true;trustServerCertificate=true;databaseName=%s;user=%s;password=%s;",
-    		other.Configuration.SQL_DATABASE,
-    		other.Configuration.SQL_USERNAME,
-    		other.Configuration.SQL_PASSWORD);
+    		other.Login.SQL_DATABASE,
+    		other.Login.SQL_USERNAME,
+    		other.Login.SQL_PASSWORD);
     
-    private String url = String.format("jdbc:sqlserver://localhost:1433;databaseName=%s;encrypt=true;trustServerCertificate=true;",other.Configuration.SQL_DATABASE);
+    private String url = String.format("jdbc:sqlserver://localhost:1433;databaseName=%s;encrypt=true;trustServerCertificate=true;",other.Login.SQL_DATABASE);
 
     // Method for getting the singleton instance of the class
     public static  DatabaseConnection getInstance() {
@@ -31,7 +31,7 @@ public class DatabaseConnection {
     // Method for getting the database connection
     public Connection getConnection() throws SQLException {
     	Connection connection;
-            connection = DriverManager.getConnection(url,other.Configuration.SQL_USERNAME,other.Configuration.SQL_PASSWORD);
+            connection = DriverManager.getConnection(url,other.Login.SQL_USERNAME,other.Login.SQL_PASSWORD);
 //        	connection = DriverManager.getConnection(CONNECTION_STRING);
         return connection;
     }
---
+++ src/other/LoginTemplate.java
@@ -0,0 +1,8 @@
+package other;
+
+public class LoginTemplate {
+	public static final String HOSTNAME = "";
+	public static final String DATABASENAME = "";
+	public static final String USERNAME = "";
+	public static final String PASSWORD = "";
+}
