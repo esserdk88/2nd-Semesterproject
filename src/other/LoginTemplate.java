3189c854e67be4a655ba954a169bf79a589ec125
Nuuurf <57993604+Nuuurf@users.noreply.github.com>
2023-04-14 09:50:51 +0200

    Update LoginTemplate.java

--- src/other/LoginTemplate.java
+++ src/other/LoginTemplate.java
@@ -1,5 +1,10 @@
 package other;
-
+/**
+ * 
+ * To Create login file created file called "Login.java"
+ * And fill out the information
+ *
+ */
 public class LoginTemplate {
 	public static final String HOSTNAME = "";
 	public static final String DATABASENAME = "";
