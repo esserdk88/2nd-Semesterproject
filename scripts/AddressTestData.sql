164539e79113606f60e7ac3948da635b01085f7f
esserdk88 <rasmus.lyngberg@gmail.com>
2023-04-20 10:34:27 +0200

    more scripts

---
+++ scripts/AddressTestData.sql
@@ -0,0 +1,8 @@
+INSERT INTO Address (address_streetname, address_streetnumber, address_zipcode, address_cityname)
+VALUES
+('Sofiendalsvej', '60', '9200', 'Aalborg SV'),
+('Hobrovej', '73B', '9530', 'Støvring'),
+('Blegkildeallé', '45', '9000', 'Aalborg'),
+('Lindholm brygge', '21', '9400', 'Nørresundby'),
+('Brenning', '15', '9400', 'Nørresundby'),
+('Godsbanen', '19 1.6', '9000', 'Aalborg')
