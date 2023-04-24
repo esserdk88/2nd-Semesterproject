1c2f7fa3025558c781c73d7e516f9ccbd85e11d4
Nuuurf <57993604+Nuuurf@users.noreply.github.com>
2023-04-24 12:51:02 +0200

    Refactored AssetOverview

--- src/gui/AssetOverview.java
+++ src/gui/AssetOverview.java
@@ -30,6 +30,8 @@ import dal.AssetDB;
 import model.Asset;
 
 public class AssetOverview extends JPanel {
+	
+	//Textfields
 	private JTextField txtSg;
 	private JTextField idTextField;
 	private JTextField headgroupTextField;
@@ -37,169 +39,65 @@ public class AssetOverview extends JPanel {
 	private JTextField subgroupTextField;
 	private JTextField serialNumberTextField;
 	private JTextField departmentTextField;
-	private JTextField textField;
+	
+	//Tables
 	private JPopupMenu popUp;
 	private DefaultTable assetTable;
-	private MainFrame mainFrame;
-	private JScrollPane assetSrollPanel;
+	private JScrollPane assetScrollPanel;
+	private JScrollPane workOrderScrollPanel;
+	private DefaultTable workOrderTable;
 	
+	//Panels
+	private JPanel topPanel;
+	private JPanel leftPanel;
+	private JPanel centerPanel;
+	private JPanel assetPanel;
+	private JPanel fillerPanel;
+	private JPanel textFieldPanel;
+	private JPanel informationTextFieldPanel;
+	private JPanel workOrderPanel;
+	private JPanel workOrderButtonPanel;
+	
+	//Labels
+	private JLabel lblNewLabel;
+	private JLabel idLabel;
+	private JLabel headGroupLabel;
+	private JLabel nameLabel;
+	private JLabel subGroupLabel;
+	private JLabel serialNumberLabel;
+	private JLabel departmentLabel;
+	private JPanel extraPanel;
+	private JLabel maintenanceIntervalLabel;
+	private JLabel extraInfoLabel;
+	
+	//Buttons
+	private JButton searchButton;
+	private JButton changeButton;
+	private JButton addNewButton;
+	private JButton editButton;
+	private JButton deleteButton;
+	
+	//Extra Components
+	private JSpinner spinner;
+	private MainFrame mainFrame;
 	private AssetDB assetDatabase;
 	private AssetController assetCtrl;
-	private ArrayList<Asset> list;
-	
+	private ArrayList<Asset> list;	
 	/**
 	 * Create the panel.
 	 * @throws SQLException 
 	 */
-	public AssetOverview(MainFrame mainFrame) throws SQLException {
+	public AssetOverview(MainFrame mainFrame) throws SQLException{
 		this.mainFrame = mainFrame;
 		setLayout(new BorderLayout(0, 0));
-		JPanel topPanel = new JPanel();
-		FlowLayout flowLayout = (FlowLayout) topPanel.getLayout();
-		flowLayout.setAlignment(FlowLayout.RIGHT);
-		add(topPanel, BorderLayout.NORTH);
-		
-		JLabel lblNewLabel = new JLabel("Søg på ID");
-		topPanel.add(lblNewLabel);
-		
-		txtSg = new JTextField();
-		txtSg.setText("Søg efter ID");
-		topPanel.add(txtSg);
-		txtSg.setColumns(10);
-		
-		JButton searchButton = new JButton("Søg");
-		topPanel.add(searchButton);
-		
-		JPanel leftPanel = new JPanel();
-		add(leftPanel, BorderLayout.WEST);
-		
-		JPanel centerPanel = new JPanel();
-		add(centerPanel, BorderLayout.CENTER);
-		centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.Y_AXIS));
-		
-		JPanel assetPanel = new JPanel();
-		centerPanel.add(assetPanel);
-		assetPanel.setLayout(new BorderLayout(0, 0));
-		assetSrollPanel = new JScrollPane();
-		assetPanel.add(assetSrollPanel, BorderLayout.CENTER);
-		
-		JPanel fillerPanel = new JPanel();
-		fillerPanel.setLayout(new GridLayout(15, 1, 5, 5));
-		assetPanel.add(fillerPanel, BorderLayout.EAST);
-		JPanel textFieldPanel = new JPanel();
-		centerPanel.add(textFieldPanel);
-		FlowLayout fl_textFieldPanel = new FlowLayout(FlowLayout.CENTER, 5, 5);
-		textFieldPanel.setLayout(fl_textFieldPanel);
-		
-		JPanel informationTextFieldPanel = new JPanel();
-		textFieldPanel.add(informationTextFieldPanel);
-		informationTextFieldPanel.setLayout(new GridLayout(3, 4, 6, 3));
-		
-		JLabel idLabel = new JLabel("ID");
-		idLabel.setHorizontalTextPosition(SwingConstants.CENTER);
-		informationTextFieldPanel.add(idLabel);
-		
-		idTextField = new JTextField();
-		informationTextFieldPanel.add(idTextField);
-		idTextField.setEnabled(false);
-		idTextField.setColumns(10);
-		
-		JLabel headGroupLabel = new JLabel("Hovedgruppe");
-		informationTextFieldPanel.add(headGroupLabel);
-		
-		headgroupTextField = new JTextField();
-		informationTextFieldPanel.add(headgroupTextField);
-		headgroupTextField.setColumns(10);
-		
-		JLabel nameLabel = new JLabel("Navn");
-		nameLabel.setHorizontalTextPosition(SwingConstants.CENTER);
-		informationTextFieldPanel.add(nameLabel);
-		
-		nameTextField = new JTextField();
-		informationTextFieldPanel.add(nameTextField);
-		nameTextField.setColumns(10);
-		
-		JLabel subGroupLabel = new JLabel("Undergruppe");
-		informationTextFieldPanel.add(subGroupLabel);
-		
-		subgroupTextField = new JTextField();
-		informationTextFieldPanel.add(subgroupTextField);
-		subgroupTextField.setColumns(10);
-		
-		JLabel serialNumberLabel = new JLabel("Serienr.");
-		serialNumberLabel.setOpaque(true);
-		serialNumberLabel.setHorizontalTextPosition(SwingConstants.CENTER);
-		informationTextFieldPanel.add(serialNumberLabel);
-		
-		serialNumberTextField = new JTextField();
-		informationTextFieldPanel.add(serialNumberTextField);
-		serialNumberTextField.setColumns(10);
-		
-		JLabel departmentLabel = new JLabel("Afdeling");
-		informationTextFieldPanel.add(departmentLabel);
-		
-		departmentTextField = new JTextField();
-		informationTextFieldPanel.add(departmentTextField);
-		departmentTextField.setColumns(10);
-		
-		JPanel extraPanel = new JPanel();
-		textFieldPanel.add(extraPanel);
-		extraPanel.setLayout(new GridLayout(2, 2, 6, 3));
-		
-		JLabel maintenanceIntervalLabel = new JLabel("Vedligeholdelsesinterval");
-		extraPanel.add(maintenanceIntervalLabel);
-		
-		JSpinner spinner = new JSpinner();
-		spinner.setModel(new SpinnerNumberModel(Integer.valueOf(0), Integer.valueOf(0), null, Integer.valueOf(1)));
-		extraPanel.add(spinner);
-		
-		JLabel extraInfoLabel = new JLabel("Interval er i dage");
-		extraPanel.add(extraInfoLabel);
-		
-		JButton changeButton = new JButton("Ændre");
-		changeButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		extraPanel.add(changeButton);
-		JPanel workOrderPanel = new JPanel();
-		centerPanel.add(workOrderPanel);
-		String[] columns2 = new String[] { "Column", "Column1", "Column2", "Column3" };
-		workOrderPanel.setLayout(new BorderLayout(0, 0));
-		
-		
-		JScrollPane workOrderScrollPanel = new JScrollPane();
-		workOrderPanel.add(workOrderScrollPanel, BorderLayout.CENTER);
-		DefaultTable workOrderTable = new DefaultTable(null, columns2);
-		workOrderScrollPanel.setViewportView(workOrderTable);
-		
-		JPanel workOrderButtonPanel = new JPanel();
-		workOrderPanel.add(workOrderButtonPanel, BorderLayout.EAST);
-		workOrderButtonPanel.setLayout(new BoxLayout(workOrderButtonPanel, BoxLayout.Y_AXIS));
-		
-		JButton addNewButton = new JButton("Tilføj ny");
-		workOrderButtonPanel.add(addNewButton);
-		addNewButton.setMaximumSize(new Dimension(80, 23));
-		addNewButton.setPreferredSize(new Dimension(80, 23));
-		addNewButton.setMinimumSize(new Dimension(30, 5));
-		
-		JButton editButton = new JButton("Rediger");
-		workOrderButtonPanel.add(editButton);
-		editButton.setMaximumSize(new Dimension(80, 23));
-		editButton.setPreferredSize(new Dimension(80, 23));
-		editButton.setMinimumSize(new Dimension(30, 5));
-		
-		JButton deleteButton = new JButton("Slet");
-		workOrderButtonPanel.add(deleteButton);
-		deleteButton.setMaximumSize(new Dimension(80, 23));
-		deleteButton.setPreferredSize(new Dimension(80, 23));
-		deleteButton.setMinimumSize(new Dimension(30, 5));
-		
-		list = fetchAllAssets();
-		setTable(list);
+		setPanels();
+		setButtons();
+		setLabelsAndTextfields();
+		setTables();
 		setPopUpMenu();
 	}
 	
+
 	private void showPopUp(MouseEvent e) {
         if (e.isPopupTrigger()) {
             popUp.show(e.getComponent(), e.getX(), e.getY());
@@ -250,6 +148,8 @@ public class AssetOverview extends JPanel {
 	}
 
 	private void setTable(ArrayList<Asset> list) {
+		assetScrollPanel = new JScrollPane();
+		assetPanel.add(assetScrollPanel, BorderLayout.CENTER);
 		boolean[] activeColumns = new boolean[] { true, true, true, false, true, true };
 		String[] columns = new String[] { "AssetID", "Navn", "Anskfaffelsesdato", "Beskrivelse", "Status",
 				"Producent" };
@@ -258,7 +158,7 @@ public class AssetOverview extends JPanel {
 		
 		assetTable = new DefaultTable(data, columns, activeColumns);
 
-		assetSrollPanel.setViewportView(assetTable);
+		assetScrollPanel.setViewportView(assetTable);
 	}
 	
 	private String[][] convertToStringArray(ArrayList<Asset> dataArrayList) {
@@ -299,6 +199,155 @@ public class AssetOverview extends JPanel {
 				mainFrame.setNewCenterPanel(readAsset);
 			}
 		}
+	}
+	private void setTables() throws SQLException {
+		String[] columns2 = new String[] { "Column", "Column1", "Column2", "Column3" };
+		workOrderScrollPanel = new JScrollPane();
+		workOrderPanel.add(workOrderScrollPanel, BorderLayout.CENTER);
+		workOrderTable = new DefaultTable(null, columns2);
+		workOrderScrollPanel.setViewportView(workOrderTable);
+		list = fetchAllAssets();
+		setTable(list);
+	}
+
+	private void setLabelsAndTextfields() {
+		lblNewLabel = new JLabel("Søg på ID");
+		topPanel.add(lblNewLabel);
+		
+		txtSg = new JTextField();
+		txtSg.setText("Søg efter ID");
+		topPanel.add(txtSg);
+		txtSg.setColumns(10);
+		idLabel = new JLabel("ID");
+		idLabel.setHorizontalTextPosition(SwingConstants.CENTER);
+		informationTextFieldPanel.add(idLabel);
+		
+		idTextField = new JTextField();
+		informationTextFieldPanel.add(idTextField);
+		idTextField.setEnabled(false);
+		idTextField.setColumns(10);
+		
+		headGroupLabel = new JLabel("Hovedgruppe");
+		informationTextFieldPanel.add(headGroupLabel);
+		
+		headgroupTextField = new JTextField();
+		informationTextFieldPanel.add(headgroupTextField);
+		headgroupTextField.setColumns(10);
+		
+		nameLabel = new JLabel("Navn");
+		nameLabel.setHorizontalTextPosition(SwingConstants.CENTER);
+		informationTextFieldPanel.add(nameLabel);
+		
+		nameTextField = new JTextField();
+		informationTextFieldPanel.add(nameTextField);
+		nameTextField.setColumns(10);
+		
+		subGroupLabel = new JLabel("Undergruppe");
+		informationTextFieldPanel.add(subGroupLabel);
+		
+		subgroupTextField = new JTextField();
+		informationTextFieldPanel.add(subgroupTextField);
+		subgroupTextField.setColumns(10);
+		
+		serialNumberLabel = new JLabel("Serienr.");
+		serialNumberLabel.setOpaque(true);
+		serialNumberLabel.setHorizontalTextPosition(SwingConstants.CENTER);
+		informationTextFieldPanel.add(serialNumberLabel);
+		
+		serialNumberTextField = new JTextField();
+		informationTextFieldPanel.add(serialNumberTextField);
+		serialNumberTextField.setColumns(10);
+		
+		departmentLabel = new JLabel("Afdeling");
+		informationTextFieldPanel.add(departmentLabel);
+		
+		departmentTextField = new JTextField();
+		informationTextFieldPanel.add(departmentTextField);
+		departmentTextField.setColumns(10);
+		
+		maintenanceIntervalLabel = new JLabel("Vedligeholdelsesinterval");
+		extraPanel.add(maintenanceIntervalLabel);
+		
+		spinner = new JSpinner();
+		spinner.setModel(new SpinnerNumberModel(Integer.valueOf(0), Integer.valueOf(0), null, Integer.valueOf(1)));
+		extraPanel.add(spinner);
+		
+		extraInfoLabel = new JLabel("Interval er i dage");
+		extraPanel.add(extraInfoLabel);
+		
+	}
+
+	private void setButtons() {
+		searchButton = new JButton("Søg");
+		topPanel.add(searchButton);
+		changeButton = new JButton("Ændre");
+		changeButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		extraPanel.add(changeButton);
+		
+		addNewButton = new JButton("Tilføj ny");
+		workOrderButtonPanel.add(addNewButton);
+		addNewButton.setMaximumSize(new Dimension(80, 23));
+		addNewButton.setPreferredSize(new Dimension(80, 23));
+		addNewButton.setMinimumSize(new Dimension(30, 5));
+		
+		editButton = new JButton("Rediger");
+		workOrderButtonPanel.add(editButton);
+		editButton.setMaximumSize(new Dimension(80, 23));
+		editButton.setPreferredSize(new Dimension(80, 23));
+		editButton.setMinimumSize(new Dimension(30, 5));
+		
+		deleteButton = new JButton("Slet");
+		workOrderButtonPanel.add(deleteButton);
+		deleteButton.setMaximumSize(new Dimension(80, 23));
+		deleteButton.setPreferredSize(new Dimension(80, 23));
+		deleteButton.setMinimumSize(new Dimension(30, 5));
+	}
+
+	private void setPanels() {
+		topPanel = new JPanel();
+		FlowLayout flowLayout = (FlowLayout) topPanel.getLayout();
+		flowLayout.setAlignment(FlowLayout.RIGHT);
+		add(topPanel, BorderLayout.NORTH);
+		
+		
+		leftPanel = new JPanel();
+		add(leftPanel, BorderLayout.WEST);
+		
+		centerPanel = new JPanel();
+		add(centerPanel, BorderLayout.CENTER);
+		centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.Y_AXIS));
+		
+		assetPanel = new JPanel();
+		centerPanel.add(assetPanel);
+		assetPanel.setLayout(new BorderLayout(0, 0));
+		fillerPanel = new JPanel();
+		fillerPanel.setLayout(new GridLayout(15, 1, 5, 5));
+		assetPanel.add(fillerPanel, BorderLayout.EAST);
+		textFieldPanel = new JPanel();
+		centerPanel.add(textFieldPanel);
+		FlowLayout fl_textFieldPanel = new FlowLayout(FlowLayout.CENTER, 5, 5);
+		textFieldPanel.setLayout(fl_textFieldPanel);
+		
+		informationTextFieldPanel = new JPanel();
+		textFieldPanel.add(informationTextFieldPanel);
+		informationTextFieldPanel.setLayout(new GridLayout(3, 4, 6, 3));
+
+		extraPanel = new JPanel();
+		textFieldPanel.add(extraPanel);
+		extraPanel.setLayout(new GridLayout(2, 2, 6, 3));	
+		
+		workOrderPanel = new JPanel();
+		centerPanel.add(workOrderPanel);
+		workOrderPanel.setLayout(new BorderLayout(0, 0));
+		
+		
+		workOrderButtonPanel = new JPanel();
+		workOrderPanel.add(workOrderButtonPanel, BorderLayout.EAST);
+		workOrderButtonPanel.setLayout(new BoxLayout(workOrderButtonPanel, BoxLayout.Y_AXIS));
+		
 	}
 
 }
