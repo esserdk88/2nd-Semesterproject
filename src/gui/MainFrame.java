ed524d763bd6b85e9739d4178303b10fb4eae26c
Nuuurf <57993604+Nuuurf@users.noreply.github.com>
2023-04-24 10:53:52 +0200

    Refactor af GUI

--- src/gui/MainFrame.java
+++ src/gui/MainFrame.java
@@ -9,9 +9,6 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Stack;
 
--- src/gui/ReadWorkOrder.java
+++ src/gui/ReadWorkOrder.java
@@ -26,6 +26,8 @@ import java.util.Calendar;
 import java.awt.Font;
 
 public class ReadWorkOrder extends JPanel {
+	
+	//Textfields
 	private JTextField txtTitle;
 	private JTextField txtInterval;
 	private JTextField txtAssetID;
@@ -35,34 +37,161 @@ public class ReadWorkOrder extends JPanel {
 	private JTextField txtRegNo;
 	private JTextField txtPriority;
 	private JTextField txtEmployeeID;
-
+	
+	//Panels
+	private JPanel southPanel;
+	private JPanel centerPanel;
+	
+	//Buttons
+	private JButton btnCancel;
+	private JButton btnSave;
+	private JButton btnAdd;
+	private JButton btnDelete;
+	
+	//Labels
+	private JLabel lblNewLabel;
+	private JLabel lblTitle;
+	private JLabel lblAssetID;
+	private JLabel lblNewLabel_2;
+	private JLabel lblNewLabel_3;
+	private JLabel lblInterval;
+	private JLabel lblNewLabel_6;
+	private JLabel lblNewLabel_4;
+	private JLabel lblNewLabel_7;
+	private JLabel lblNewLabel_8;
+	private JLabel lblNewLabel_1;
+	private JLabel lblNewLabel_5;
+	
+	//Tabel
+	private JScrollPane historyScollPane;
+	private DefaultTable historyTable;
+	private JScrollPane scrollPane;
+	private DefaultTable completedActionsTable;
+	
+	//Extra
+	private JScrollPane descriptionScrollPane;
+	private JTextArea textArea;
+	private JCheckBox checkDate;
+	private JSpinner spinner;
+	
 	/**
 	 * Create the panel.
 	 */
 	public ReadWorkOrder() {
 		setLayout(new BorderLayout(0, 0));
 		
-		JPanel southPanel = new JPanel();
-		FlowLayout fl_southPanel = (FlowLayout) southPanel.getLayout();
-		fl_southPanel.setAlignment(FlowLayout.RIGHT);
-		add(southPanel, BorderLayout.SOUTH);
+		setPanels();
+		setLabelsAndTextFields();
+		setTables();
+		setButtons();
+		setSpinners();
+		setCheckBoxes();
+	}
+
+	private void setCheckBoxes() {
+		checkDate = new JCheckBox("Lukkes dato");
+		checkDate.setHorizontalAlignment(SwingConstants.TRAILING);
+		checkDate.setSelected(true);
+		GridBagConstraints gbc_checkDate = new GridBagConstraints();
+		gbc_checkDate.anchor = GridBagConstraints.WEST;
+		gbc_checkDate.insets = new Insets(0, 0, 5, 5);
+		gbc_checkDate.gridx = 7;
+		gbc_checkDate.gridy = 6;
+		centerPanel.add(checkDate, gbc_checkDate);
+	}
+
+	private void setSpinners() {
+		SimpleDateFormat spinnerModel = new SimpleDateFormat("dd.MM.yyyy");
+		spinner = new JSpinner();
+		spinner.setModel(new SpinnerDateModel());
+		spinner.setEditor(new JSpinner.DateEditor(spinner, spinnerModel.toPattern()));
+		GridBagConstraints gbc_spinner = new GridBagConstraints();
+		gbc_spinner.fill = GridBagConstraints.HORIZONTAL;
+		gbc_spinner.insets = new Insets(0, 0, 5, 5);
+		gbc_spinner.gridx = 8;
+		gbc_spinner.gridy = 6;
+		centerPanel.add(spinner, gbc_spinner);
+	}
+
+	private void setButtons() {
 		
-		JButton btnCancel = new JButton("Udskyd arbejdsordre");
+		btnCancel = new JButton("Udskyd arbejdsordre");
 		southPanel.add(btnCancel);
 		
-		JButton btnSave = new JButton("Færddiggør arbejdsordre");
+		btnSave = new JButton("Færddiggør arbejdsordre");
 		southPanel.add(btnSave);
 		
-		JPanel centerPanel = new JPanel();
-		add(centerPanel, BorderLayout.NORTH);
-		GridBagLayout gbl_centerPanel = new GridBagLayout();
-		gbl_centerPanel.columnWidths = new int[]{0, 0, 101, 39, 55, 101, 59, 88, 0, 0, 0};
-		gbl_centerPanel.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-		gbl_centerPanel.columnWeights = new double[]{1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, Double.MIN_VALUE};
-		gbl_centerPanel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE};
-		centerPanel.setLayout(gbl_centerPanel);
+		btnAdd = new JButton("Tilføj ny");
+		GridBagConstraints gbc_btnAdd = new GridBagConstraints();
+		gbc_btnAdd.fill = GridBagConstraints.HORIZONTAL;
+		gbc_btnAdd.insets = new Insets(0, 0, 5, 0);
+		gbc_btnAdd.gridx = 9;
+		gbc_btnAdd.gridy = 9;
+		centerPanel.add(btnAdd, gbc_btnAdd);
 		
-		JLabel lblNewLabel = new JLabel("Beskrivelse");
+		btnDelete = new JButton("Slet");
+		GridBagConstraints gbc_btnDelete = new GridBagConstraints();
+		gbc_btnDelete.insets = new Insets(0, 0, 5, 0);
+		gbc_btnDelete.fill = GridBagConstraints.HORIZONTAL;
+		gbc_btnDelete.gridx = 9;
+		gbc_btnDelete.gridy = 10;
+		centerPanel.add(btnDelete, gbc_btnDelete);
+		
+		//btnCancel.addActionListener(e -> AddMethodToCallHere);
+		//btnSave.addActionListener(e -> AddMethodToCallHere);
+		//btnAdd.addActionListener(e -> AddMethodToCallHere);
+		//btnDelete.addActionListener(e -> AddMethodToCallHere);
+	}
+
+	private void setTables() {
+		historyScollPane = new JScrollPane();
+		historyScollPane.setPreferredSize(new Dimension(10, 0)); //Changes size of table
+		GridBagConstraints gbc_historyScollPane = new GridBagConstraints();
+		gbc_historyScollPane.gridheight = 5;
+		gbc_historyScollPane.gridwidth = 2;
+		gbc_historyScollPane.insets = new Insets(0, 0, 5, 5);
+		gbc_historyScollPane.fill = GridBagConstraints.BOTH;
+		gbc_historyScollPane.gridx = 1;
+		gbc_historyScollPane.gridy = 7;
+		centerPanel.add(historyScollPane, gbc_historyScollPane);
+		
+		//TODO update historyScollPane
+		String[] columnsHistory = new String[] { "Reg. nr", "Start dato", "Lukket dato" };
+		historyTable = new DefaultTable(null, columnsHistory);
+		historyScollPane.setViewportView(historyTable);
+		
+		scrollPane = new JScrollPane();
+		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
+		gbc_scrollPane.gridheight = 3;
+		gbc_scrollPane.gridwidth = 5;
+		gbc_scrollPane.insets = new Insets(0, 0, 5, 5);
+		gbc_scrollPane.fill = GridBagConstraints.BOTH;
+		gbc_scrollPane.gridx = 4;
+		gbc_scrollPane.gridy = 9;
+		centerPanel.add(scrollPane, gbc_scrollPane);
+		
+		//TODO update completedActionsTable
+		String[] columnsCompletedActions = new String[] { "Reg. af", "Dato", "Bemærkning" };
+		completedActionsTable = new DefaultTable(null, columnsCompletedActions);
+		scrollPane.setViewportView(completedActionsTable);
+	}
+
+	private void setLabelsAndTextFields() {
+		
+		textArea = new JTextArea();
+		textArea.setWrapStyleWord(true);
+		textArea.setLineWrap(true);
+		descriptionScrollPane = new JScrollPane(textArea);
+		GridBagConstraints gbc_descriptionScrollPane = new GridBagConstraints();
+		gbc_descriptionScrollPane.gridheight = 3;
+		gbc_descriptionScrollPane.gridwidth = 5;
+		gbc_descriptionScrollPane.insets = new Insets(0, 0, 5, 5);
+		gbc_descriptionScrollPane.fill = GridBagConstraints.BOTH;
+		gbc_descriptionScrollPane.gridx = 4;
+		gbc_descriptionScrollPane.gridy = 2;
+		centerPanel.add(descriptionScrollPane, gbc_descriptionScrollPane);
+		
+		lblNewLabel = new JLabel("Beskrivelse");
 		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
 		gbc_lblNewLabel.anchor = GridBagConstraints.WEST;
 		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
@@ -70,7 +199,7 @@ public class ReadWorkOrder extends JPanel {
 		gbc_lblNewLabel.gridy = 1;
 		centerPanel.add(lblNewLabel, gbc_lblNewLabel);
 		
-		JLabel lblTitle = new JLabel("Emne");
+		lblTitle = new JLabel("Emne");
 		GridBagConstraints gbc_lblTitle = new GridBagConstraints();
 		gbc_lblTitle.anchor = GridBagConstraints.WEST;
 		gbc_lblTitle.insets = new Insets(0, 0, 5, 5);
@@ -87,7 +216,7 @@ public class ReadWorkOrder extends JPanel {
 		centerPanel.add(txtTitle, gbc_txtTitle);
 		txtTitle.setColumns(10);
 		
-		JLabel lblAssetID = new JLabel("Maskine ID");
+		lblAssetID = new JLabel("Maskine ID");
 		GridBagConstraints gbc_lblAssetID = new GridBagConstraints();
 		gbc_lblAssetID.anchor = GridBagConstraints.WEST;
 		gbc_lblAssetID.insets = new Insets(0, 0, 5, 5);
@@ -104,20 +233,7 @@ public class ReadWorkOrder extends JPanel {
 		gbc_txtAssetID.gridy = 3;
 		centerPanel.add(txtAssetID, gbc_txtAssetID);
 		
-		JTextArea textArea = new JTextArea();
-		textArea.setWrapStyleWord(true);
-		textArea.setLineWrap(true);
-		JScrollPane descriptionScrollPane = new JScrollPane(textArea);
-		GridBagConstraints gbc_descriptionScrollPane = new GridBagConstraints();
-		gbc_descriptionScrollPane.gridheight = 3;
-		gbc_descriptionScrollPane.gridwidth = 5;
-		gbc_descriptionScrollPane.insets = new Insets(0, 0, 5, 5);
-		gbc_descriptionScrollPane.fill = GridBagConstraints.BOTH;
-		gbc_descriptionScrollPane.gridx = 4;
-		gbc_descriptionScrollPane.gridy = 2;
-		centerPanel.add(descriptionScrollPane, gbc_descriptionScrollPane);
-		
-		JLabel lblNewLabel_2 = new JLabel("Navn");
+		lblNewLabel_2 = new JLabel("Navn");
 		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
 		gbc_lblNewLabel_2.anchor = GridBagConstraints.WEST;
 		gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 5);
@@ -134,7 +250,7 @@ public class ReadWorkOrder extends JPanel {
 		gbc_txtName.gridy = 4;
 		centerPanel.add(txtName, gbc_txtName);
 		
-		JLabel lblNewLabel_3 = new JLabel("Serienr.");
+		lblNewLabel_3 = new JLabel("Serienr.");
 		GridBagConstraints gbc_lblNewLabel_3 = new GridBagConstraints();
 		gbc_lblNewLabel_3.anchor = GridBagConstraints.WEST;
 		gbc_lblNewLabel_3.insets = new Insets(0, 0, 5, 5);
@@ -151,7 +267,7 @@ public class ReadWorkOrder extends JPanel {
 		centerPanel.add(txtSerialNumber, gbc_txtSerialNumber);
 		txtSerialNumber.setColumns(10);
 		
-		JLabel lblInterval = new JLabel("Interval");
+		lblInterval = new JLabel("Interval");
 		GridBagConstraints gbc_lblInterval = new GridBagConstraints();
 		gbc_lblInterval.anchor = GridBagConstraints.WEST;
 		gbc_lblInterval.insets = new Insets(0, 0, 5, 5);
@@ -168,7 +284,7 @@ public class ReadWorkOrder extends JPanel {
 		centerPanel.add(txtInterval, gbc_txtInterval);
 		txtInterval.setColumns(10);
 		
-		JLabel lblNewLabel_6 = new JLabel("Type");
+		lblNewLabel_6 = new JLabel("Type");
 		GridBagConstraints gbc_lblNewLabel_6 = new GridBagConstraints();
 		gbc_lblNewLabel_6.anchor = GridBagConstraints.WEST;
 		gbc_lblNewLabel_6.insets = new Insets(0, 0, 5, 5);
@@ -185,7 +301,7 @@ public class ReadWorkOrder extends JPanel {
 		centerPanel.add(txtType, gbc_txtType);
 		txtType.setColumns(10);
 		
-		JLabel lblNewLabel_4 = new JLabel("Historik");
+		lblNewLabel_4 = new JLabel("Historik");
 		lblNewLabel_4.setFont(new Font("Tahoma", Font.BOLD, 12));
 		GridBagConstraints gbc_lblNewLabel_4 = new GridBagConstraints();
 		gbc_lblNewLabel_4.gridwidth = 2;
@@ -194,7 +310,7 @@ public class ReadWorkOrder extends JPanel {
 		gbc_lblNewLabel_4.gridy = 6;
 		centerPanel.add(lblNewLabel_4, gbc_lblNewLabel_4);
 		
-		JLabel lblNewLabel_7 = new JLabel("Reg nr.");
+		lblNewLabel_7 = new JLabel("Reg nr.");
 		GridBagConstraints gbc_lblNewLabel_7 = new GridBagConstraints();
 		gbc_lblNewLabel_7.anchor = GridBagConstraints.WEST;
 		gbc_lblNewLabel_7.insets = new Insets(0, 0, 5, 5);
@@ -211,44 +327,7 @@ public class ReadWorkOrder extends JPanel {
 		centerPanel.add(txtRegNo, gbc_txtRegNo);
 		txtRegNo.setColumns(10);
 		
-		JCheckBox checkDate = new JCheckBox("Lukkes dato");
-		checkDate.setHorizontalAlignment(SwingConstants.TRAILING);
-		checkDate.setSelected(true);
-		GridBagConstraints gbc_checkDate = new GridBagConstraints();
-		gbc_checkDate.anchor = GridBagConstraints.WEST;
-		gbc_checkDate.insets = new Insets(0, 0, 5, 5);
-		gbc_checkDate.gridx = 7;
-		gbc_checkDate.gridy = 6;
-		centerPanel.add(checkDate, gbc_checkDate);
-		
-		SimpleDateFormat spinnerModel = new SimpleDateFormat("dd.MM.yyyy");
-		JSpinner spinner = new JSpinner();
-		spinner.setModel(new SpinnerDateModel());
-		spinner.setEditor(new JSpinner.DateEditor(spinner, spinnerModel.toPattern()));
-		GridBagConstraints gbc_spinner = new GridBagConstraints();
-		gbc_spinner.fill = GridBagConstraints.HORIZONTAL;
-		gbc_spinner.insets = new Insets(0, 0, 5, 5);
-		gbc_spinner.gridx = 8;
-		gbc_spinner.gridy = 6;
-		centerPanel.add(spinner, gbc_spinner);
-		
-		JScrollPane historyScollPane = new JScrollPane();
-		historyScollPane.setPreferredSize(new Dimension(10, 0)); //Changes size of table
-		GridBagConstraints gbc_historyScollPane = new GridBagConstraints();
-		gbc_historyScollPane.gridheight = 5;
-		gbc_historyScollPane.gridwidth = 2;
-		gbc_historyScollPane.insets = new Insets(0, 0, 5, 5);
-		gbc_historyScollPane.fill = GridBagConstraints.BOTH;
-		gbc_historyScollPane.gridx = 1;
-		gbc_historyScollPane.gridy = 7;
-		centerPanel.add(historyScollPane, gbc_historyScollPane);
-		
-		//TODO update historyScollPane
-		String[] columnsHistory = new String[] { "Reg. nr", "Start dato", "Lukket dato" };
-		DefaultTable historyTable = new DefaultTable(null, columnsHistory);
-		historyScollPane.setViewportView(historyTable);
-		
-		JLabel lblNewLabel_8 = new JLabel("Prioritering");
+		lblNewLabel_8 = new JLabel("Prioritering");
 		GridBagConstraints gbc_lblNewLabel_8 = new GridBagConstraints();
 		gbc_lblNewLabel_8.anchor = GridBagConstraints.WEST;
 		gbc_lblNewLabel_8.insets = new Insets(0, 0, 5, 5);
@@ -265,7 +344,7 @@ public class ReadWorkOrder extends JPanel {
 		centerPanel.add(txtPriority, gbc_txtPriority);
 		txtPriority.setColumns(10);
 		
-		JLabel lblNewLabel_1 = new JLabel("Lukkes af");
+		lblNewLabel_1 = new JLabel("Lukkes af");
 		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
 		gbc_lblNewLabel_1.anchor = GridBagConstraints.WEST;
 		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
@@ -282,7 +361,7 @@ public class ReadWorkOrder extends JPanel {
 		centerPanel.add(txtEmployeeID, gbc_txtEmployeeID);
 		txtEmployeeID.setColumns(10);
 		
-		JLabel lblNewLabel_5 = new JLabel("Aktioner udført");
+		lblNewLabel_5 = new JLabel("Aktioner udført");
 		lblNewLabel_5.setFont(new Font("Tahoma", Font.BOLD, 12));
 		GridBagConstraints gbc_lblNewLabel_5 = new GridBagConstraints();
 		gbc_lblNewLabel_5.gridwidth = 5;
@@ -290,38 +369,23 @@ public class ReadWorkOrder extends JPanel {
 		gbc_lblNewLabel_5.gridx = 4;
 		gbc_lblNewLabel_5.gridy = 8;
 		centerPanel.add(lblNewLabel_5, gbc_lblNewLabel_5);
+	}
+
+	private void setPanels() {
+		southPanel = new JPanel();
+		FlowLayout fl_southPanel = (FlowLayout) southPanel.getLayout();
+		fl_southPanel.setAlignment(FlowLayout.RIGHT);
+		add(southPanel, BorderLayout.SOUTH);
 		
-		JTable test = new JTable();
-		JScrollPane scrollPane = new JScrollPane(test);
-		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
-		gbc_scrollPane.gridheight = 3;
-		gbc_scrollPane.gridwidth = 5;
-		gbc_scrollPane.insets = new Insets(0, 0, 5, 5);
-		gbc_scrollPane.fill = GridBagConstraints.BOTH;
-		gbc_scrollPane.gridx = 4;
-		gbc_scrollPane.gridy = 9;
-		centerPanel.add(scrollPane, gbc_scrollPane);
+		centerPanel = new JPanel();
+		add(centerPanel, BorderLayout.NORTH);
+		GridBagLayout gbl_centerPanel = new GridBagLayout();
+		gbl_centerPanel.columnWidths = new int[]{0, 0, 101, 39, 55, 101, 59, 88, 0, 0, 0};
+		gbl_centerPanel.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_centerPanel.columnWeights = new double[]{1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, Double.MIN_VALUE};
+		gbl_centerPanel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE};
+		centerPanel.setLayout(gbl_centerPanel);
 		
-		//TODO update completedActionsTable
-		String[] columnsCompletedActions = new String[] { "Reg. af", "Dato", "Bemærkning" };
-		DefaultTable completedActionsTable = new DefaultTable(null, columnsCompletedActions);
-		scrollPane.setViewportView(completedActionsTable);
-		
-		JButton btnAdd = new JButton("Tilføj ny");
-		GridBagConstraints gbc_btnAdd = new GridBagConstraints();
-		gbc_btnAdd.fill = GridBagConstraints.HORIZONTAL;
-		gbc_btnAdd.insets = new Insets(0, 0, 5, 0);
-		gbc_btnAdd.gridx = 9;
-		gbc_btnAdd.gridy = 9;
-		centerPanel.add(btnAdd, gbc_btnAdd);
-		
-		JButton btnDelete = new JButton("Slet");
-		GridBagConstraints gbc_btnDelete = new GridBagConstraints();
-		gbc_btnDelete.insets = new Insets(0, 0, 5, 0);
-		gbc_btnDelete.fill = GridBagConstraints.HORIZONTAL;
-		gbc_btnDelete.gridx = 9;
-		gbc_btnDelete.gridy = 10;
-		centerPanel.add(btnDelete, gbc_btnDelete);
 	}
 
 }
