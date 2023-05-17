85b3a0c3441f178bd27109790c6f361000102d3d
NikolaiCS <113992103+NikolaiCS@users.noreply.github.com>
2023-05-17 22:00:34 +0200

    fixed the fucking switchEmployeeWorkorders test

--- src/dalStubs/StubWorkOrderDB.java
+++ src/dalStubs/StubWorkOrderDB.java
@@ -8,6 +8,7 @@ import java.util.List;
 import dal.interfaces.ReferenceDBIF;
 import dal.interfaces.WorkOrderDBIF;
 import dao.Database;
+import model.Address;
 import model.Asset;
 import model.Employee;
 import model.Maintenance;
@@ -41,6 +42,29 @@ public class StubWorkOrderDB implements WorkOrderDBIF{
 	private static Repair repair;
 	private static Reference reference;
 	
+	//Employees
+	//Employee Fields 1
+			private static int employeeID = 1000000;
+			private static String cpr = "00000001";
+			private static Calendar startDateEmp = Calendar.getInstance();
+			private static String position = "Testpos";
+			private static String name = "TestName";
+			private static String phone = "00000001";
+			private static String email = "test@test.com";
+			//pls dont fuck up
+			private static Address address = null;
+			
+			//Employee Fields 1
+			private static int employeeID2 = 2000000;
+			private static String cpr2 = "00000002";
+			private static Calendar startDateEmp2 = Calendar.getInstance();
+			private static String position2 = "Testpos2";
+			private static String name2 = "TestNameTheThird";
+			private static String phone2 = "00000002";
+			private static String email2 = "test@test.com";
+			//pls dont fuck up
+			private static Address address2 = null;
+	
 	private static ReferenceDBIF referenceDB = Database.getInstance().getReferenceDataBase();
 	
 	public StubWorkOrderDB() {
@@ -48,6 +72,8 @@ public class StubWorkOrderDB implements WorkOrderDBIF{
 		reference = referenceDB.findReferenceByID(11111111);
 		maintenance = new Maintenance(repeated, intervalDayCount, 0, title, "Maintenance", startDate, endDate, priority, description, finished, sparepartsUsed, asset, null, measurements);
 		repair = new Repair(price, reference, 0, title, "Repair", startDate, endDate, priority, description, finished, sparepartsUsed, asset, null, measurements);
+		maintenance.setEmployee(new Employee(employeeID, cpr, startDateEmp, position, name, phone, email, address));
+		repair.setEmployee(new Employee(employeeID2, cpr2, startDateEmp2, position2, name2, phone2, email2, address2));
 	}
 	
 
