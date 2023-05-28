842030ef4134a1e6f752de0f01f722c6a73880a1
Rasmus Lyngberg <113756357+esserdk88@users.noreply.github.com>
2023-05-28 15:00:28 +0200

    Java doc - controllers

--- src/controller/ConnectionController.java
+++ src/controller/ConnectionController.java
@@ -3,8 +3,16 @@ package controller;
 import controller.interfaces.ConnectionControllerIF;
 import dao.DatabaseConnection;
 
+/**
+ * The ConnectionController class checks if there is a connection to the database.
+ */
 public class ConnectionController implements ConnectionControllerIF {
 
+	/**
+	 * The function checks if there is a connection to the database.
+	 * 
+	 * @return A boolean value indicating whether the database connection is currently established or not.
+	 */
 	@Override
 	public boolean isConnected() {
 		return DatabaseConnection.getInstance().isConnected();
--- src/controller/ServiceController.java
+++ src/controller/ServiceController.java
@@ -11,12 +11,28 @@ import model.Reference;
 import model.Service;
 import model.Workorder;
 
+/**
+ * The ServiceController class extends WorkOrderController and implements ServiceControllerIF,
+ * providing methods to create, find, and retrieve work orders related to services.
+ */
 public class ServiceController extends WorkOrderController implements ServiceControllerIF {
 	
+	// A constructor for the ServiceController class that calls the constructor of its superclass
+	// (WorkOrderController) using the `super()` keyword. This ensures that any initialization code in the
+	// superclass constructor is executed before the ServiceController constructor.
 	public ServiceController() {
 		super();
 	}
 	
+	/**
+	 * This Java function creates a work order for a service and throws an exception if the asset is null.
+	 * 
+	 * @param service The parameter "service" is an object of the Service class, which contains
+	 * information about a service request made by a customer. This method is responsible for creating a
+	 * work order for the service request.
+	 * @return The method is returning a boolean value, which indicates whether the creation of a work
+	 * order for the given service was successful or not.
+	 */
 	@Override
 	public boolean createWorkOrder(Service service) {
 		// we have to check if the asset is null.
@@ -25,6 +41,21 @@ public class ServiceController extends WorkOrderController implements ServiceCon
 		}			
 		return workOrderDB.addServiceWorkOrder(service);
 	}
+
+	/**
+	 * This function creates a work order for a given asset with specified details and a reference.
+	 * 
+	 * @param assetID The ID of the asset for which the work order is being created.
+	 * @param title A string representing the title of the work order.
+	 * @param startDate A Calendar object representing the start date of the work order.
+	 * @param priority A short value representing the priority of the work order. The higher the value,
+	 * the higher the priority.
+	 * @param description A brief description of the work order that needs to be created.
+	 * @param referenceCVR The referenceCVR parameter is an integer representing the CVR (Central Business
+	 * Register) number of the company that the work order is associated with.
+	 * @return The method is returning a boolean value, which indicates whether the creation of the work
+	 * order was successful or not.
+	 */
 	public boolean createWorkOrder(int assetID, String title, Calendar startDate, short priority, String description, int referenceCVR) throws SQLException {
 		AssetController assetCtr = new AssetController();
 		Asset asset = assetCtr.findAssetByID(assetID);
@@ -46,11 +77,25 @@ public class ServiceController extends WorkOrderController implements ServiceCon
 		return workOrderDB.addServiceWorkOrder(service);
 	}
 
+	/**
+	 * This Java function finds a service work order by its ID using a work order database.
+	 * 
+	 * @param workOrderID The parameter "workOrderID" is an integer value that represents the unique
+	 * identifier of a work order. This method is used to find a service work order by its ID.
+	 * @return A Service object is being returned.
+	 */
 	@Override
 	public Service findWorkOrderByID(int workOrderID) {
 		return workOrderDB.findServiceWorkOrderByID(workOrderID);
 	}
 
+	/**
+	 * This function returns a list of all work orders associated with a given asset ID.
+	 * 
+	 * @param assetID The assetID parameter is an integer value that represents the unique identifier of
+	 * an asset for which we want to retrieve all the work orders.
+	 * @return A list of Workorder objects that correspond to a specific asset ID.
+	 */
 	@Override
 	public List<Workorder> getAllWorkOrdersByAssetID(int assetID) {
 		return super.getAllWorkOrdersByAssetID(assetID);
